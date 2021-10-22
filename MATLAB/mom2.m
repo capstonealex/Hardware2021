52f4d771dc28b8bd31b56b1f8032a4c2e25397db
Musa-hub97 <92367899+Musa-hub97@users.noreply.github.com>
2021-10-22 12:45:03 +1100

    fix mom values

--- MATLAB/mom2.m
+++ MATLAB/mom2.m
@@ -1,4 +1,4 @@
 function MB = mom2(rBA, FA, rBC, FC, rBGe2, me2, rBGp2, mp2, rBGe3, me3, rBGp3, mp3, g, R10, R12)
-MB=cross(rBA,R10*FA) +cross(R12*rBC,R12*FC) +cross(rBGe2,R10*[0;0;-me2*g]) +cross(rBGp2,R10*[0;0;-mp2*g]) +cross(R12*rBGe3,R10*[0;0;-me3*g]) +cross(R12*rBGp3,R10*[0;0;-mp3*g]);
+MB=cross(rBA,R10*-FA) +cross(R12*rBC,R12*FC) +cross(rBGe2,R10*[0;0;-me2*g]) +cross(rBGp2,R10*[0;0;-mp2*g]) +cross(R12*rBGe3,R10*[0;0;-me3*g]) +cross(R12*rBGp3,R10*[0;0;-mp3*g]);
 end
 
--- MATLAB/mom6.m
+++ MATLAB/mom6.m
@@ -1,5 +1,5 @@
 function MF = mom6(R21, R10, R32, R43, R54, R56, rFE, FE, rFG, FG, rFGe6, me6, rFGe7, me7, mp7, rFGp7, g)
 R50=R54*R43*R32*R21*R10;
-MF=cross(rFE,R54*FE) +cross(R56*rFG,R56*FG) +cross(rFGe6,R50*[0;0;-me6*g]) +cross(R56*rFGe7,R50*[0;0;-me7*g]) +cross(R56*rFGp7,R50*[0;0;-mp7*g]);
+MF=cross(rFE,R54*-FE) +cross(R56*rFG,R56*FG) +cross(rFGe6,R50*[0;0;-me6*g]) +cross(R56*rFGe7,R50*[0;0;-me7*g]) +cross(R56*rFGp7,R50*[0;0;-mp7*g]);
 end
 
